718240427b6f0f16e116cf5be50c3470aeae0f42
hidayattaufiqur <htaufiqurrahma2001@gmail.com>
2021-04-25 04:25:42 +0800

    update

--- server.js
+++ server.js
@@ -48,5 +48,8 @@ app.get('/articles/blog', async (req, res) => {
 app.use('/articles', articleRouter)
 app.use('/mail', Form)
 
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 app.listen(5000)
-app.listen(process.env.PORT || port, () => console.log('example app is listening at http//localhost:${port}'))
+// app.listen(process.env.PORT || port, () => console.log('example app is listening at http//localhost:${port}'))
